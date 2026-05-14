317f4892451ed64b956886acda5428b63dad207f
Matthias Bender <mbender1309@googlemail.com>
2026-05-14 17:57:40 +0200

    test: add registerUser helper function

--- tests/e2e/helpers/user-journey-helpers.ts
+++ tests/e2e/helpers/user-journey-helpers.ts
@@ -7,4 +7,14 @@ interface RecipeData {
   ingredients: Array<{ name: string; quantity: number }>;
 }
 
-// Helper functions will be added in following tasks
+/**
+ * Register a new user and verify successful registration (redirect to dashboard)
+ */
+export const registerUser = (email: string, password: string): void => {
+  cy.visit('http://localhost:3000/register');
+  cy.get('input[type="email"]').type(email);
+  cy.get('input[type="password"]').first().type(password);
+  cy.get('input[type="password"]').last().type(password);
+  cy.get('button[type="submit"]').click();
+  cy.url().should('include', '/dashboard');
+};
